80ccc1855a2c3d6eb1fffc68182078831559a1d1
gmatolo <geraldmuli7@gmail.com>
2022-08-10 14:39:58 +0300

    imports additional libraries

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+
+# Import libraries
+import requests
+from bs4 import BeautifulSoup
 from PyPDF2 import PdfReader
+
+
+reader = PdfReader("./forms/test_form_1.pdf")
+# with open("./forms/test.pdf", "rb") as fh
+pages = reader.pages[0]
+print(pages.extract_text())
